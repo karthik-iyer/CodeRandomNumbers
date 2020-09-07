3a6c7bfa0bb2571cca55d38eec3a1a1bdd0fdfa9
Karthik Iyer <karthikiyer@Karthiks-Air.attlocal.net>
2020-09-06 23:27:38 -0500

    Program to generate list of random numbers between 1 and 10000 both inclusive

---
+++ RandomNumberList/RandomListGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomNumberList
+{
+    class RandomListGenerator
+    {
+        static void Main(string[] args)
+        {
+            int numberOfItems = 1;
+            const int MAXLISTCOUNT = 10000;
+            var RandomGeneratorList = new HashSet<int>();
+
+            while(numberOfItems <= MAXLISTCOUNT)
+            {
+                var random = new Random().Next(1,10001);
+                if(!RandomGeneratorList.Contains(random))
+                {
+                    RandomGeneratorList.Add(random);
+                    numberOfItems++;
+                }
+            }
+            numberOfItems = 1;
+
+            foreach(var item in RandomGeneratorList)
+            {
+                Console.WriteLine($" count : {numberOfItems++} , Value : {item}");
+            }
+        }
+    }
+}
